9ce77405ff6321a3312e0401cc6bcf138e6f289a
HeinFritz <heinfritz42@gmail.com>
2025-05-09 02:20:35 +0100

    Projects Data

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,5 +2,5 @@ class Reward < ApplicationRecord
   belongs_to :project
   validates :name, :amount, :delivery_date, presence: true
   has_one_attached :image
-  has_many :pledges
+  has_many :pledges, dependent: :destroy
 end
